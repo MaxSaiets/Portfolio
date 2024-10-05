aec5c33a51b39dc46098ef9936516f7a975a6980
Max Saiets <114159679+MaxSaiets@users.noreply.github.com>
2024-10-05 16:42:04 +0300

    Fix routing

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -3,20 +3,23 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { publicRoutes } from "./routes";
 import { MAIN_ROUTE } from "./utils/consts";
 import { v4 as uuidv4 } from "uuid"; 
+import { HashRouter } from 'react-router-dom';
 
 const AppRouter = () => {
 
     return (
-        <Routes> 
-            {publicRoutes.map(({path, Component}) => 
-                <Route key={uuidv4()} path={path} element={<Component />} exact/> // exact - означає що шлях повинен точно співпадати
-            )}
+        <HashRouter>
+            <Routes> 
+                {publicRoutes.map(({path, Component}) => 
+                    <Route key={uuidv4()} path={path} element={<Component />} exact/> // exact - означає що шлях повинен точно співпадати
+                )}
 
-            <Route
-                path="*"
-                element={<Navigate to={MAIN_ROUTE} replace />}
-            />
-        </Routes>
+                <Route
+                    path="*"
+                    element={<Navigate to={MAIN_ROUTE} replace />}
+                />
+            </Routes>
+        </HashRouter>
     );
 };
 
